29c1f6c5e1d83926997e9f598b811f6b635e6b9c
Tonci Marinac <toncimarinac@gmail.com>
2021-11-23 00:26:55 +0100

    Added tranform rotate to the car

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Driver : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        
+        transform.Rotate( 0, 0, 45 );
     }
 
     // Update is called once per frame
