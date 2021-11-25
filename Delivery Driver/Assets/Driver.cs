30be1905473b5e635b1928f8bafe8d574c3dc614
Tonci Marinac <toncimarinac@gmail.com>
2021-11-25 22:53:51 +0100

    Player controls with added frame rate independence

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour {
     // Start is called before the first frame update
+
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 15f;
+
     void Start() {
-        transform.Rotate( 0, 0, 45 );
+        //transform.Rotate( 0, 0, 45 );
     }
 
     // Update is called once per frame
     void Update() {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
